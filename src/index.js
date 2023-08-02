e6efd66e8de017bfdf567913ec0e113d3da0f515
Kleostro <maxim_zabaluev@icloud.com>
2023-08-03 01:45:51 +0300

    feat: completed the task in the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
-}
+    n = String(n);
+    if(n.indexOf('-')) {
+        return n.split('').reverse().join('');
+    } else {
+        return n.substring(1).split('').reverse().join('');
+    };
+};
